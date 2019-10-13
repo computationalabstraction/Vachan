579df03ae4d669765f077fc977b093225ac2a511
archanpatkar <pat@archanpatkar.com>
2019-10-13 16:36:24 +0530

    Formatting

--- __tests__/spec.test.js
+++ __tests__/spec.test.js
@@ -2,15 +2,14 @@ const promisesAplusTests = require("promises-aplus-tests");
 const v = require("../src/vachan");
 const P = v.P;
 
-function deferred()
-{
+function deferred() {
     let p = new P();
     return {
-        promise:p,
-        resolve:(v) => p.resolve(v),
-        reject:(v) => p.reject(v)
+        promise: p,
+        resolve: (v) => p.resolve(v),
+        reject: (v) => p.reject(v)
     };
-}      
+}
 
 const adapter = {};
 adapter.resolved = v.P.resolve;
@@ -18,4 +17,4 @@ adapter.rejected = v.P.reject;
 adapter.deferred = deferred;
 
 P.vachanify(promisesAplusTests)(adapter)
-.then(_ => console.log("Passing the Promise/A+ Specification Compliance Test Suite"));
+    .then(_ => console.log("Passing the Promise/A+ Specification Compliance Test Suite"));
